07121ae34f36f1dcd9d9933a2e9a862e7b324d31
coolwind0202 <erable0226@gmail.com>
2025-09-29 14:25:57 +0900

    add: implement searching by name and summaryw

---
+++ app/_components/content.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Input } from "@heroui/input";
+import { Syllabus } from "@/app/syllabus";
+import z from "zod";
+import { useFuse } from "../_hooks/use_fuse";
+import { useState } from "react";
+
+export type Props = {
+  syllabus: z.infer<typeof Syllabus>
+};
+
+export function Content({ syllabus }: Props) {
+  const [word, setWord] = useState("");
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWord(event.target.value);
+  };
+
+  const { search } = useFuse(syllabus.subjects, { keys: ["name", "summary"] });
+  const searchResult = search(word);
+
+  return (
+    <div className="p-8 flex flex-col gap-6">
+      <section>
+        <Input
+          placeholder="例: 光エレクトロニクス"
+          label="検索ワード"
+          onChange={handleSearchChange}
+        />
+      </section>
+      <ul className="flex flex-col gap-6">
+        {searchResult.map((entry) => {
+          const subject = entry.item;
+          return (
+            <li key={`${subject.category}_${subject.name}`}>
+              <h1 className="font-bold text-2xl">{subject.name}</h1>
+              <p>{subject.summary}</p>
+            </li>
+          )
+        })
+        }
+      </ul>
+    </div>
+  );
+}
---
+++ app/_hooks/use_fuse.ts
@@ -0,0 +1,13 @@
+import Fuse, { IFuseOptions } from "fuse.js";
+import { useCallback } from "react";
+
+export function useFuse<T>(arr: T[], options: IFuseOptions<T>) {
+  const search = useCallback((pattern: string) => {
+    const fuse = new Fuse(arr, options);
+    return fuse.search(pattern);
+  }, [arr, options]);
+
+  return {
+    search
+  }
+}
