f1fad87738fe3cdee9939d0bd083978a4f601597
coolwind0202 <erable0226@gmail.com>
2025-09-29 11:12:56 +0900

    add: list page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import { Input } from "@heroui/input";
+import { fetchSyllabus } from "./fetcher";
+
+export default async function Home() {
+  const syllabus = await fetchSyllabus();
+
   return (
-    <p>Hello world.</p>
+    <div className="p-8 flex flex-col gap-3">
+      <section>
+        <Input placeholder="例: 光エレクトロニクス" label="検索ワード" />
+      </section>
+      <ul className="flex flex-col gap-6">
+        {syllabus.subjects.map((subject) => (
+          <li key={`${subject.category}_${subject.name}`}>
+            <h1 className="font-bold text-2xl">{subject.name}</h1>
+            <p>{subject.summary}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
