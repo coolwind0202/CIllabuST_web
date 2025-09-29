c281a973340325e8e57acb42b417fb39e7fd5dcc
coolwind0202 <erable0226@gmail.com>
2025-09-29 17:59:55 +0900

    chore: extract subject and category types to another file

---
+++ app/_types/syllabus.ts
@@ -0,0 +1,6 @@
+import { Syllabus } from "../syllabus"
+import z from "zod"
+
+export type Syllabus = z.infer<typeof Syllabus>
+export type Subject = Syllabus["subjects"][number]
+export type SubjectCategory = Subject["category"]
